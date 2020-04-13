ce8580f1c2bb8cf2f992d56048ce6f783e83c364
mohamadbastin <mohamadbastin@gmail.com>
2020-04-13 21:57:10 +0430

    report still

--- apiv1/views.py
+++ apiv1/views.py
@@ -214,20 +214,24 @@ class FormParticipantListView(ListAPIView):
 class ParticipantAnsweredFormView(ListCreateAPIView):
     permission_classes = [IsAuthenticated]
     serializer_class = AnsweredFormSerializer
+
     # allowed_methods = ['GET', 'POST']
 
     def get_queryset(self):
         # 2020 - 04 - 13
-        date = self.request.data.get('date')
+        date = self.request.data.get('date', None)
         formid = self.kwargs.get("formid")
         participant = self.kwargs.get("ppid")
 
-        if date != 0 or date != "0":
-            year = int(date[0, 4])
-            month = int(date[5, 7])
-            day = int(date[8,])
-            return AnsweredForm.objects.filter(form=formid, participant=participant, date__year=year, date__month=month,
-                                               date__day=day).order_by("-date")
+        if date:
+            if date != 0 or date != "0":
+                year = int(date[0, 4])
+                month = int(date[5, 7])
+                day = int(date[8,])
+
+                return AnsweredForm.objects.filter(form=formid, participant=participant, date__year=year,
+                                                   date__month=month,
+                                                   date__day=day).order_by("-date")
 
         return AnsweredForm.objects.filter(form=formid, participant=participant).order_by("-date")
 
